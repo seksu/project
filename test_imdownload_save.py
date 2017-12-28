3322de746f8e834d1d2de5b4ce41f6b856cf3534
seksu <jarukit.suchat@gmail.com>
2017-12-29 00:19:28 +0700

    Add files via upload

--- test_imdownload_save.py
+++ test_imdownload_save.py
@@ -15,10 +15,10 @@ def store_raw_images():
         try:
             print(i)
             urllib.request.urlretrieve(i, "neg/"+str(pic_num)+".jpg")
-            img = cv2.imread("neg/"+str(pic_num)+".jpg",cv2.IMREAD_GRAYSCALE)
+            img = cv2.imread("neg/"+str(pic_num)+".jpg")
             # should be larger than samples / pos pic (so we can place our image on it)
-            resized_image = cv2.resize(img, (100, 100))
-            cv2.imwrite("neg/"+str(pic_num)+".jpg",resized_image)
+            #resized_image = cv2.resize(img, (100, 100))
+            cv2.imwrite("neg/"+str(pic_num)+".jpg",img)
             pic_num += 1
             
         except Exception as e:
